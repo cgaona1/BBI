cc9c0f3c3a635c791cb87f86261a8033a3f6b9b0
cgaona1 <gaonachris930@gmail.com>
2022-10-21 19:51:45 -0600

    Added a sign in page for the project

--- bottles/views.py
+++ bottles/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django_tables2 import SingleTableView
 
 from .models import Bottle
 from .tables import BottleTable
 
-class BottlesListView(SingleTableView):
+
+class BottlesListView(LoginRequiredMixin, SingleTableView):
+    login_url = '/accounts/login/'
     model = Bottle
     template_name = 'list.html'
     table_class = BottleTable
